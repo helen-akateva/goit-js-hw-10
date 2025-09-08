d01467a9077a2363e143e2ea2326e40805bf458f
helen-akateva <akareva17@gmail.com>
2025-09-08 16:20:03 +0300

    update

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -13,7 +13,57 @@ const refs = {
   secondsEl: document.querySelector('span[data-seconds]'),
 };
 
+refs.buttonEl.disabled = true;
+
 let userSelectedDate;
+let intervalId;
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+function updateTimer() {
+  const timeDifference = userSelectedDate.getTime() - Date.now();
+
+  if (timeDifference <= 0) {
+    clearInterval(intervalId);
+    refs.inputEl.disabled = false;
+    refs.buttonEl.disabled = true;
+
+    refs.dataEl.textContent = '00';
+    refs.hoursEl.textContent = '00';
+    refs.minutesEl.textContent = '00';
+    refs.secondsEl.textContent = '00';
+  } else {
+    const timeComponents = convertMs(timeDifference);
+
+    refs.dataEl.textContent = addLeadingZero(timeComponents.days);
+    refs.hoursEl.textContent = addLeadingZero(timeComponents.hours);
+    refs.minutesEl.textContent = addLeadingZero(timeComponents.minutes);
+    refs.secondsEl.textContent = addLeadingZero(timeComponents.seconds);
+  }
+}
+
+function handleButtonClick() {
+  refs.buttonEl.disabled = true;
+  refs.inputEl.disabled = true;
+  intervalId = setInterval(updateTimer, 1000);
+}
+
+refs.buttonEl.addEventListener('click', handleButtonClick);
 
 const options = {
   enableTime: true,
